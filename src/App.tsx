74a9732f736ddc54d221ae2b26ef5abadc1f6f81
Nadirol <99162550+Nadirol@users.noreply.github.com>
2022-11-29 17:40:58 +0700

    set up

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header"
+
+const App: any = () => {
+  return (
+    <div className="">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+
+const Header = (): JSX.Element => {
+    return (
+        <header>
+            <ul className="flex">
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
